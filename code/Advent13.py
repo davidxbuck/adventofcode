60daa9889dfa5e76f37cd2126e0fa8bcb6e951eb
David Buck <david.x.buck@icloud.com>
2019-01-03 11:34:36 +0000

    Day 13 Part 2 complete. Refactored part 1

--- code/Advent13.py
+++ code/Advent13.py
@@ -1,13 +1,14 @@
 # Advent of Code 2018 Day 13
 #
-# Read file and extract dependencies
-
-file = open("Advent13.txt", 'r')
-input = [row[:-1] for row in file]
 
 from operator import add
 
 import numpy as np
+
+# Read file and extract dependencies
+
+file = open("Advent13.txt", 'r')
+input = [row[:-1] for row in file]
 
 
 def visualise_grid():
@@ -23,70 +24,48 @@ gridsize = 150
 grid = np.empty([gridsize, gridsize], dtype=str)
 carts = []
 dir = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+arrows = "^>v<"
 
 # populate grid with track and extract carts
 
 for x in range(gridsize):
     for y in range(len(input[x])):
-        if input[x][y] in "^":  # [cartno[0], position[1], direction[2], next turn[3]]
-            carts.append([len(carts), [x, y], 0, -1])
-            grid[x, y] = "|"
-        elif input[x][y] in ">":
-            carts.append([len(carts), [x, y], 1, -1])
-            grid[x, y] = "-"
-        elif input[x][y] in "v":
-            carts.append([len(carts), [x, y], 2, -1])
-            grid[x, y] = "|"
-        elif input[x][y] in "<":
-            carts.append([len(carts), [x, y], 3, -1])
-            grid[x, y] = "-"
-
+        if input[x][y] in arrows:  # [cartno[0], position[1], direction[2], next turn[3]]
+            carts.append([len(carts), [x, y], arrows.index(input[x][y]), -1])
+            grid[x, y] = input[x][y].translate(str.maketrans(arrows, "|-|-"))
         else:
             grid[x, y] = input[x][y]
 
 numcarts = len(carts)
-loop = 0
+tick = 0
 
 crash = False
 
 while not crash:
+    tick += 1
     move_order = [y[0] for y in sorted(carts, key=lambda x: x[1])]
-    new_scratch = [None for _ in range(numcarts)]
-    new_positions = [None for _ in range(numcarts)]
+
     for move in move_order:
-        new_positions[move] = list(map(add, carts[move][1], dir[carts[move][2]]))
-        newgrid = grid[new_positions[move][0], new_positions[move][1]]
-        new_scratch[move] = newgrid
+        new_position = list(map(add, carts[move][1], dir[carts[move][2]]))
+        newgrid = grid[new_position[0], new_position[1]]
         for k in range(numcarts):
-            if carts[k][1] == new_positions[move]:
-                crash_position = [new_positions[move][1], new_positions[move][0]]
+            if carts[k][1] == new_position:
+                crash_position = [new_position[1], new_position[0]]
                 crash = True
                 break
         if crash == True:
             break
-        if newgrid == "/":  # ^ becomes >, > becomes ^, < becomes v, v becomes <
-            if carts[move][2] == 0:
-                carts[move][2] = 1
-            elif carts[move][2] == 1:
-                carts[move][2] = 0
-            elif carts[move][2] == 2:
-                carts[move][2] = 3
-            elif carts[move][2] == 3:
-                carts[move][2] = 2
-        if newgrid == "\\":  # ^ becomes <, > becomes v, < becomes ^, v becomes >
-            if carts[move][2] == 0:
-                carts[move][2] = 3
-            elif carts[move][2] == 1:
-                carts[move][2] = 2
-            elif carts[move][2] == 2:
-                carts[move][2] = 1
-            elif carts[move][2] == 3:
-                carts[move][2] = 0
+        if newgrid == "/":  # ^0 becomes >1, >1 becomes ^0, v2 becomes <3, <3 becomes v2
+            carts[move][2] = (5 - carts[move][2]) % 4
+
+        if newgrid == "\\":  # ^0 becomes <3, >1 becomes v2, v2 becomes >1, <3 becomes ^0
+            carts[move][2] = (3 - carts[move][2]) % 4
+
         if newgrid == "+":
             carts[move][2] = (carts[move][2] + carts[move][3]) % 4  # new direction
             carts[move][3] = (carts[move][3] + 2) % 3 - 1  # next direction
-        carts[move][1] = new_positions[move]
+        carts[move][1] = new_position
 
 visualise_grid()
 
-print("Part 1 - Crash", crash_position, "evaluated after", loop, "iterations")
+print("Part 1 - Crash", crash_position, "evaluated after", tick, "iterations")

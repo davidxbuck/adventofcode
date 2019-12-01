aa8e17a69547411e50c2b38fac19c5674e937e83
David Buck <david.buck@datascience.fun>
2019-12-01 12:31:25 +0000

    Advent of Code Day 1 - part 2 refactored with recursive solution

--- src/Advent2019-01.py
+++ src/Advent2019-01.py
@@ -3,13 +3,8 @@
 # From https://adventofcode.com/2019/day/1
 #
 weights = list(map(int, [weight.strip() for weight in open('../inputs2019/Advent2019-01.txt', 'r')]))
-fuelcalc = lambda x : x//3 - 2
-new_weights = list(map(fuelcalc, weights))
-total_fuel = sum(new_weights)
-print(f'AoC 2019 Day 1, Part 1 answer is {total_fuel}')
+fuelcalc = lambda x: x // 3 - 2
+print(f'AoC 2019 Day 1, Part 1 answer is {sum(map(fuelcalc, weights))}')
 
-while sum(new_weights) > 0:
-    new_weights = [x for x in list(map(fuelcalc, new_weights)) if x > 0]
-    total_fuel += sum(new_weights)
-
-print(f'AoC 2019 Day 1, Part 2 answer is {total_fuel}')
+fuel = lambda x: x > 0 and fuelcalc(x) + max(fuel(fuelcalc(x)), 0)
+print(f'AoC 2019 Day 1, Part 2 answer is {sum(map(fuel, weights))}')

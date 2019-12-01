b7db3db800f2142c29c4b076390c40a07d46c342
David Buck <david.buck@datascience.fun>
2019-12-01 18:09:02 +0000

    Advent of Code Day 1 - part 2 added single-line-of-code solution

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
-# Hi
+# Advent of Code 2019
+
+[Twitter](https://twitter.com/datascience_fun)
+
+[![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+
+## What is Advent of Code?
+[Advent of Code](http://adventofcode.com) is an Advent calendar of small programming puzzles, created by [Eric Wastl](https://twitter.com/ericwastl). Each year, starting on Dec 1st, an advent calendar of small programming puzzles are unlocked once a day at 05:00 (UTC).
--- src/Advent2019-01.py
+++ src/Advent2019-01.py
@@ -10,4 +10,4 @@ fuel_recursive = lambda x: x > 0 and (y := fuel(x)) + max(fuel_recursive(y), 0)
 print(f'AoC 2019 Day 1, Part 2 answer is {sum(map(fuel_recursive, masses))}')
 
 # One line answer... Might be slighly overlong for PEP-8
-# print(f'AoC 2019 Day 1, Part 1 answer is {sum(map((fuel := lambda x: x // 3 - 2), (masses := list(map(int, [mass.strip() for mass in open("../inputs2019/Advent2019-01.txt", "r")])))))}\nAoC 2019 Day 1, Part 2 answer is {sum(map((fuel_recursive := lambda x: x > 0 and (y := fuel(x)) + max(fuel_recursive(y), 0)), masses))}')
+print(f'#1: {sum(map((fuel := lambda x: x // 3 - 2), (masses := list(map(int, [mass.strip() for mass in open("../inputs2019/Advent2019-01.txt", "r")])))))}\n#2: {sum(map((fuel_recursive := lambda x: x > 0 and (y := fuel(x)) + max(fuel_recursive(y), 0)), masses))}')

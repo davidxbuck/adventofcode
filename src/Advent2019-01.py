e4fa28796c28ace429a6d8ed117b693d12d374cf
David Buck <david.buck@datascience.fun>
2019-12-01 16:10:27 +0000

    Advent of Code Day 1 - part 2 added single-line-of-code solution

--- src/Advent2019-01.py
+++ src/Advent2019-01.py
@@ -2,12 +2,12 @@
 #
 # From https://adventofcode.com/2019/day/1
 #
-weights = list(map(int, [weight.strip() for weight in open('../inputs2019/Advent2019-01.txt', 'r')]))
-fuelcalc = lambda x: x // 3 - 2
-print(f'AoC 2019 Day 1, Part 1 answer is {sum(map(fuelcalc, weights))}')
+masses = list(map(int, [mass.strip() for mass in open('../inputs2019/Advent2019-01.txt', 'r')]))
+fuel = lambda x: x // 3 - 2
+print(f'AoC 2019 Day 1, Part 1 answer is {sum(map(fuel, masses))}')
 
-fuel = lambda x: x > 0 and (y := fuelcalc(x)) + max(fuel(y), 0)
-print(f'AoC 2019 Day 1, Part 2 answer is {sum(map(fuel, weights))}')
+fuel_recursive = lambda x: x > 0 and (y := fuel(x)) + max(fuel_recursive(y), 0)
+print(f'AoC 2019 Day 1, Part 2 answer is {sum(map(fuel_recursive, masses))}')
 
 # One line answer... Might be slighly overlong for PEP-8
-# print(f'AoC 2019 Day 1, Part 1 answer is {sum(map((fuelcalc := lambda x: x // 3 - 2), (weights := list(map(int, [weight.strip() for weight in open("../inputs2019/Advent2019-01.txt", "r")])))))}\nAoC 2019 Day 1, Part 2 answer is {sum(map((fuel := lambda x: x > 0 and (y := fuelcalc(x)) + max(fuel(y), 0)), weights))}')
+# print(f'AoC 2019 Day 1, Part 1 answer is {sum(map((fuel := lambda x: x // 3 - 2), (masses := list(map(int, [mass.strip() for mass in open("../inputs2019/Advent2019-01.txt", "r")])))))}\nAoC 2019 Day 1, Part 2 answer is {sum(map((fuel_recursive := lambda x: x > 0 and (y := fuel(x)) + max(fuel_recursive(y), 0)), masses))}')

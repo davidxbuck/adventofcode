43474400d8333c50a8ce0c3a6db4e70f2caed7ef
David Buck <david.buck@datascience.fun>
2019-12-01 16:03:24 +0000

    Advent of Code Day 1 - part 2 added single-line-of-code solution

--- src/Advent2019-01.py
+++ src/Advent2019-01.py
@@ -8,3 +8,6 @@ print(f'AoC 2019 Day 1, Part 1 answer is {sum(map(fuelcalc, weights))}')
 
 fuel = lambda x: x > 0 and (y := fuelcalc(x)) + max(fuel(y), 0)
 print(f'AoC 2019 Day 1, Part 2 answer is {sum(map(fuel, weights))}')
+
+# One line answer... Might be slighly overlong for PEP-8
+# print(f'AoC 2019 Day 1, Part 1 answer is {sum(map((fuelcalc := lambda x: x // 3 - 2), (weights := list(map(int, [weight.strip() for weight in open("../inputs2019/Advent2019-01.txt", "r")])))))}\nAoC 2019 Day 1, Part 2 answer is {sum(map((fuel := lambda x: x > 0 and (y := fuelcalc(x)) + max(fuel(y), 0)), weights))}')

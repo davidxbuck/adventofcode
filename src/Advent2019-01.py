61001b10b6af2935afb63959cb1c9fe2e37c1b40
David Buck <david.buck@datascience.fun>
2019-12-02 10:31:26 +0000

    Advent of Code Day 2 - complete

--- src/Advent2019-01.py
+++ src/Advent2019-01.py
@@ -9,5 +9,5 @@ print(f'AoC 2019 Day 1, Part 1 answer is {sum(map(fuel, masses))}')
 fuel_recursive = lambda x: x > 0 and (y := fuel(x)) + max(fuel_recursive(y), 0)
 print(f'AoC 2019 Day 1, Part 2 answer is {sum(map(fuel_recursive, masses))}')
 
-# One line answer... Might be slighly overlong for PEP-8
+# One line answer... Might be slightly overlong for PEP-8
 print(f'#1: {sum(map((fuel := lambda x: x // 3 - 2), (masses := list(map(int, [mass.strip() for mass in open("../inputs2019/Advent2019-01.txt", "r")])))))}\n#2: {sum(map((fuel_recursive := lambda x: x > 0 and (y := fuel(x)) + max(fuel_recursive(y), 0)), masses))}')
---
+++ src/Advent2019-02.py
@@ -0,0 +1,74 @@
+# Advent of Code 2019
+#
+# From https://adventofcode.com/2019/day/2
+#
+
+NOUN = 12
+VERB = 2
+OUTPUT = 19690720
+
+
+class Intcode(object):
+    def __init__(self, program, noun, verb, pointer=0):
+        self.pointer = pointer
+        self.program = program
+        self.program[1:3] = noun, verb
+        self.terminated = False
+
+    def cmd1(self):
+        cmd, augend, addend, sum_ = self.program[self.pointer:self.pointer + 4]
+        self.program[sum_] = self.program[augend] + self.program[addend]
+
+    def cmd2(self):
+        multiplicand, multiplier, product = self.program[self.pointer + 1:self.pointer + 4]
+        self.program[product] = self.program[multiplicand] * self.program[multiplier]
+
+    def cmd99(self):
+        self.terminated = True
+
+    def run(self):
+        while not self.terminated:
+            # print(f"Executing: {self.pointer} {self.program[self.pointer:self.pointer + 4]}")
+            exec(f"self.cmd{self.program[self.pointer]}()")
+            self.pointer += 4
+        return self.program[0]
+
+
+def iterate_noun(inputs, noun_, verb):
+    for noun in range(noun_, 99):
+        result = Intcode(inputs[:], noun, verb).run()
+        if result >= OUTPUT:
+            if result > OUTPUT:
+                noun -= 1
+            return noun, verb, result
+
+
+def iterate_verb(inputs, noun, verb_):
+    for verb in range(verb_, 99):
+        result = Intcode(inputs[:], noun, verb).run()
+        if result >= OUTPUT:
+            if result > OUTPUT:
+                verb -= 1
+            return noun, verb, result
+
+
+def main():
+    inputs = list(map(int, [mass.strip().split(',') for mass in open('../inputs2019/Advent2019-02.txt', 'r')][0]))
+
+    print(f'AoC 2019 Day 2, Part 1 answer is {Intcode(inputs[:], NOUN, VERB).run()}')
+
+    # determine whether to iterate nouns or verbs first, whichever has the greatest effect on result:
+    if Intcode(inputs[:], NOUN + 1, VERB).run() > Intcode(inputs[:], NOUN, VERB + 1).run():
+        noun, verb, result = iterate_noun(inputs[:], NOUN, VERB)
+        noun, verb, result = iterate_verb(inputs[:], noun, VERB)
+    else:
+        noun, verb, result = iterate_verb(inputs[:], NOUN, VERB)
+        noun, verb, result = iterate_noun(inputs[:], NOUN, verb)
+    if Intcode(inputs[:], noun, verb).run() == OUTPUT:
+        print(f'AoC 2019 Day 2, Part 2 answer is {noun * 100 + verb}')
+    else:
+        print(f'Error')
+
+
+if __name__ == '__main__':
+    main()

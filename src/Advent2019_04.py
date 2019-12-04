7c808d3744c823675f40575fb2f4c5aa380db8e8
David Buck <david.buck@datascience.fun>
2019-12-04 18:08:55 +0000

    2019 day 4 working - additional version

--- src/Advent2019_04.py
+++ src/Advent2019_04.py
@@ -7,17 +7,37 @@ import re
 START = 136760
 STOP = 595730
 
-part1 = part2 = 0
 
-for i in range(START, STOP + 1):
-    test = str(i)
-    repeats = re.findall(r'(.)\1+', test)
-    if repeats and test == "".join(sorted(test)):
-        part1 += 1
-        for repeat in repeats:
-            if test.count(repeat) == 2:
-                part2 += 1
-                break
+def first_version():
+    part1 = part2 = 0
+    for i in range(START, STOP + 1):
+        test = str(i)
+        repeats = re.findall(r'(.)\1+', test)
+        if repeats and test == "".join(sorted(test)):
+            part1 += 1
+            for repeat in repeats:
+                if test.count(repeat) == 2:
+                    part2 += 1
+                    break
+    print(f'AoC 2019 Day 4, Part 1: {part1}')
+    print(f'AoC 2019 Day 4, Part 2: {part2}')
 
-print(f'AoC 2019 Day 4, Part 1: {part1}')
-print(f'AoC 2019 Day 4, Part 2: {part2}')
+
+# For good measure (and borrowed initially from @brotherluii
+
+def second_version():
+    part1 = part2 = 0
+    for number in range(START, STOP + 1):
+        digits = str(number)
+        adjacent = any(digits[i] == digits[i + 1] for i in range(5))
+        is_increasing = adjacent and all(digits[i] <= digits[i + 1] for i in range(5))
+        part1 += is_increasing
+        doubles = is_increasing and any(digits.count(check) == 2 for check in set(digits))
+        part2 += doubles
+    print(f'AoC 2019 Day 4, Part 1: {part1}')
+    print(f'AoC 2019 Day 4, Part 2: {part2}')
+
+
+if __name__ == '__main__':
+    first_version()
+    second_version()

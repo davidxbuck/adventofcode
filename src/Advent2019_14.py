54dc487cc050ed310835969e9a1b83aeed905bab
David Buck <david.buck@datascience.fun>
2019-12-14 22:59:55 +0000

    Day 14 Complete - changes for efficiency, and variable name changes for clarity

--- src/Advent2019_14.py
+++ src/Advent2019_14.py
@@ -1,52 +1,52 @@
 from math import ceil
 
 
-def calculate_ore(reactions, excesses, search):
-    excess = dict(excesses)
-    excess['FUEL'] = -search
+def calculate_ore(reactions, chem_qty, fuel_required):
+    chem_qty['FUEL'] = -fuel_required
     ore = 0
-    excess['ORE'] = 0
     satisfied = False
     while not satisfied:
         satisfied = True
-        for key in excess.keys():
-            if key == "ORE" and excess[key] < 0:
-                ore -= excess[key]
-                excess[key] = 0
+        for output_chemical in chem_qty.keys():
+            if output_chemical == "ORE" and chem_qty[output_chemical] < 0:
+                ore -= chem_qty[output_chemical]
+                chem_qty[output_chemical] = 0
                 continue
-            if excess[key] < 0:
+            if chem_qty[output_chemical] < 0:
                 satisfied = False
-                mult = ceil(abs(excess[key]) / reactions[key][0])
-                excess[key] += reactions[key][0] * mult
-                for subkey in reactions[key][1].keys():
-                    qty = reactions[key][1][subkey]
-                    excess[subkey] -= qty * mult
+                output_qty, input_chemicals = reactions[output_chemical]
+                batches = ceil(abs(chem_qty[output_chemical]) / output_qty)
+                chem_qty[output_chemical] += output_qty * batches
+                for input_chemical in input_chemicals.keys():
+                    qty = input_chemicals[input_chemical]
+                    chem_qty[input_chemical] -= qty * batches
     return ore
 
 
 def main():
     with open('../inputs2019/Advent2019_14.txt', 'r') as f:
-        # with open('../tests/test_Advent2019_14d.txt', 'r') as f:
         equations = [x.strip().split() for x in f]
     reactions = {}
-    excesses = {}
+    chem_qty = {}
     for equation in equations:
-        result = [equation[-1], int(equation[-2])]
+        chemical, quantity = equation[-1], int(equation[-2])
         inputs = {y.strip(','): int(x) for x, y in zip(equation[0:-3:2], equation[1:-3:2])}
-        reactions[result[0]] = [result[1], inputs]
-        excesses[result[0]] = 0
-    search = 1
-    print(f'AoC 2019 Day 14, Part 1: Ore consumed {calculate_ore(reactions, excesses, search)}')
+        reactions[chemical] = [quantity, inputs]
+        chem_qty[chemical] = 0
+    chem_qty['ORE'] = 0
+    fuel_required = 1
+    print(f'AoC 2019 Day 14, Part 1: Ore consumed {calculate_ore(reactions, chem_qty, fuel_required)}')
+
     adjustment = 2 ** 40
-    search = 1000000
+    fuel_required = 1000000
     while adjustment > 0:
-        ore = calculate_ore(reactions, excesses, search)
+        ore = calculate_ore(reactions, chem_qty, fuel_required)
         if ore > 1000000000000:
-            search = search - adjustment
+            fuel_required = fuel_required - adjustment
         else:
-            search = search + adjustment
+            fuel_required = fuel_required + adjustment
         adjustment //= 2
-    print(f'AoC 2019 Day 14, Part 2: Fuel produced {search}')
+    print(f'AoC 2019 Day 14, Part 2: Fuel produced {fuel_required}')
 
 
 if __name__ == '__main__':

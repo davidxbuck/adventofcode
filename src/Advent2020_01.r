cbfc8f56b3624a26328babe4f3cac42c0a56372d
David Buck <david.x.buck@icloud.com>
2020-12-01 22:52:23 +0000

    Some optimisation

--- src/Advent2020_01.r
+++ src/Advent2020_01.r
@@ -6,20 +6,17 @@ inp <- paste(readLines('~/Documents/CodeRepositories/AdventofCode/inputs2020/Adv
 inp <- strsplit(inp, split = ",")
 inp <- as.integer(inp[[1]])
 
-combos <- as_tibble(gtools::combinations(n = 200, r = 2, v = inp, repeats.allowed = FALSE))
-combos <- combos %>%
+combos <- as_tibble(gtools::combinations(n = 200, r = 2, v = inp)) %>%
   rowwise() %>%
-  mutate(total = sum(c(V1, V2))) %>%
-  filter(total == 2020) %>%
+  filter(sum(c(V1, V2)) == 2020) %>%
   mutate(product = V1 * V2)
 
 print(stringr::str_interp("AoC 2020 Day 1, Part 1 answer is (${V1}, ${V2}) is ${product}", combos[1,]))
 
-combos <- as_tibble(gtools::combinations(n = 200, r = 3, v = inp, repeats.allowed = FALSE))
-combos <- combos %>%
+
+combos <- as_tibble(gtools::combinations(n = 200, r = 3, v = inp)) %>%
   rowwise() %>%
-  mutate(total = sum(c(V1, V2, V3))) %>%
-  filter(total == 2020) %>%
+  filter(sum(c(V1, V2, V3)) == 2020) %>%
   mutate(product = V1 * V2 * V3)
 
 print(stringr::str_interp("AoC 2020 Day 1, Part 2 answer is (${V1}, ${V2}, ${V3}) is ${product}", combos[1,]))

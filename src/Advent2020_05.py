7b65737a7af23a805f719a891f42e6f909179c99
David Buck <david.x.buck@icloud.com>
2020-12-05 06:32:56 +0000

    2020 Day 5 Efficiency tweak

--- src/Advent2020_05.py
+++ src/Advent2020_05.py
@@ -6,13 +6,13 @@
 def get_seats(filename=''):
     inputs = (row for row in open(f'../inputs2020/Advent2020_05{filename}.txt', 'r'))
     seats = map(lambda x: x.translate(str.maketrans("FBRL", "0110")), inputs)
-    return list(map(lambda x: int(x[:7], 2) * 8 + int(x[7:], 2), seats))
+    return set(map(lambda x: int(x[:7], 2) * 8 + int(x[7:], 2), seats))
 
 
 def find_seat(occupied_seats):
-    empty_seats = set(range(128 * 8)).difference(occupied_seat_set := set(occupied_seats))
+    empty_seats = set(range(128 * 8)).difference(occupied_seats)
     for possibility in empty_seats:
-        if possibility - 1 in occupied_seat_set and possibility + 1 in occupied_seat_set:
+        if possibility - 1 in occupied_seats and possibility + 1 in occupied_seats:
             return possibility
 
 

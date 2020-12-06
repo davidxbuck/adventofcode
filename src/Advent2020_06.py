5f956871b1be4cbad3f12afd35e9eaccea67b506
David Buck <david.x.buck@icloud.com>
2020-12-06 08:10:40 +0000

    2020 Day 6 streamlined

--- src/Advent2020_06.py
+++ src/Advent2020_06.py
@@ -2,26 +2,7 @@
 #
 # From https://adventofcode.com/2020/day/6
 #
-import string
 
-def get_questionnaires(filename=''):
-    answers = (set(row.strip()) for row in open(f'../inputs2020/Advent2020_06{filename}.txt', 'r'))
-
-    part1 = part2 = 0
-    part1_set, part2_set = set(), set(string.ascii_lowercase)
-    for answer in answers:
-        if answer:
-            part1_set = part1_set.union(answer)
-            part2_set = part2_set.intersection(answer)
-        else:
-            part1 += len(part1_set)
-            part2 += len(part2_set)
-            part1_set, part2_set = set(), set(string.ascii_lowercase)
-    if answer:
-        part1 += len(part1_set)
-        part2 += len(part2_set)
-    return part1, part2
-
-part1, part2 = get_questionnaires()
-print(f"""AoC 2020 Day 6 Part 1 answer is: {part1}""")
-print(f"""AoC 2020 Day 6 Part 2 answer is: {part2}""")
+data = [list(map(set, x.split('\n'))) for x in open('../inputs2020/Advent2020_06.txt', 'r').read().split('\n\n')]
+print(f"""AoC 2020 Day 6 Part 1 answer is: {sum(len(set.union(*x)) for x in data)}""")
+print(f"""AoC 2020 Day 6 Part 2 answer is: {sum(len(set.intersection(*x)) for x in data)}""")

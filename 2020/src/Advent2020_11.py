948803f772f937771684d9ca2396d22919cb062a
David Buck <david.x.buck@icloud.com>
2020-12-12 07:54:19 +0000

    2020 Day 11 complete.

--- 2020/src/Advent2020_11.py
+++ 2020/src/Advent2020_11.py
@@ -27,24 +27,13 @@ while not (padded_seats == prev_seats).all():
                     next_seats[x, y] = padded_seats[x, y]
     padded_seats = next_seats.copy()
 
-print(np.count_nonzero(padded_seats == '#'))
+print(f"""AoC 2020 Day 11 Part 1 answer is: {np.count_nonzero(padded_seats == '#')}""")
 
-padded = np.array(seats.shape) + [2, 2]
-padded_seats = np.empty(padded, dtype=str)
 padded_seats[1:padded[0] - 1, 1:padded[1] - 1] = seats.copy()
 next_seats = np.empty_like(padded_seats, dtype=str)
 prev_seats = np.empty_like(padded_seats, dtype=str)
-x_to, y_to = np.array(seats.shape) + 1
 
-NW = [[-1 * x, 1 * x] for x in range(1, max(seats.shape))]
-N = [[0, 1 * x] for x in range(1, max(seats.shape))]
-NE = [[1 * x, 1 * x] for x in range(1, max(seats.shape))]
-E = [[1 * x, 0] for x in range(1, max(seats.shape))]
-SE = [[1 * x, -1 * x] for x in range(1, max(seats.shape))]
-S = [[0, -1 * x] for x in range(1, max(seats.shape))]
-SW = [[-1 * x, -1 * x] for x in range(1, max(seats.shape))]
-W = [[-1 * x, 0] for x in range(1, max(seats.shape))]
-angles = [NW, N, NE, E, SE, S, SW, W]
+directions = [(-1, 1), (0, 1), (1, 1), (1, 0,), (1, -1), (0, -1), (-1, -1), (-1, 0)]
 
 while not (padded_seats == prev_seats).all():
     prev_seats = padded_seats.copy()
@@ -52,11 +41,15 @@ while not (padded_seats == prev_seats).all():
         for y in range(1, y_to):
             if padded_seats[x, y] in ['#', 'L']:
                 occupied = 0
-                for angle in angles:
-                    for ang_x, ang_y in angle:
-                        if 1 <= x + ang_x <= x_to and 1 <= y + ang_y <= y_to:
-                            if padded_seats[x + ang_x, y + ang_y] in ['#', 'L']:
-                                occupied += int(padded_seats[x + ang_x, y + ang_y] == '#')
+                for ang_x, ang_y in directions:
+                    dist = 0
+                    while True:
+                        dist += 1
+                        x_check = x + ang_x * dist
+                        y_check = y + ang_y * dist
+                        if 1 <= x_check <= x_to and 1 <= y_check <= y_to:
+                            if padded_seats[x_check, y_check] in ['#', 'L']:
+                                occupied += int(padded_seats[x_check, y_check] == '#')
                                 break
                         else:
                             break
@@ -68,4 +61,4 @@ while not (padded_seats == prev_seats).all():
                     next_seats[x, y] = padded_seats[x, y]
     padded_seats = next_seats.copy()
 
-print(np.count_nonzero(padded_seats == '#'))
+print(f"""AoC 2020 Day 11 Part 2 answer is: {np.count_nonzero(padded_seats == '#')}""")

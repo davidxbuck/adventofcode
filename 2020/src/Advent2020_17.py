67f03093b0e8f5030763606c10c05d1a8af2dd73
David Buck <david.x.buck@icloud.com>
2020-12-17 07:54:34 +0000

    2020 Day 17 working

---
+++ 2020/src/Advent2020_17.py
@@ -0,0 +1,48 @@
+# Advent of Code 2020
+#
+# From https://adventofcode.com/2020/day/17
+#
+import numpy as np
+
+loops = 6
+padding = loops + 1
+printing = False
+filename = ''
+
+# Inputs
+cubes = np.array(list(list(row.strip()) for row in open(f'../inputs/Advent2020_17{filename}.txt', 'r')))
+array_size = [cubes.shape[0] + padding * 2, cubes.shape[1] + padding * 2, padding * 2 + 1]
+cube_grid = np.full(array_size, '.', dtype=str)
+cube_grid[padding:array_size[0] - padding, padding:array_size[1] - padding, padding] = cubes.copy()
+
+# Part 1
+# Should really set the ranges so that each run only operates over x,y,z space that might contain #, and expand
+for _ in range(loops):
+    new_grid = np.full(array_size, '.', dtype=str)
+    for x in range(1, array_size[0] - 1):
+        for y in range(1, array_size[1] - 1):
+            for z in range(1, array_size[2] - 1):
+                minicube = cube_grid[x - 1: x + 2, y - 1:y + 2, z - 1: z + 2]
+                if (minicube[1, 1, 1] == "#" and np.count_nonzero(minicube == "#") in [3, 4]) or (
+                        minicube[1, 1, 1] == "." and np.count_nonzero(minicube == "#") == 3):
+                    new_grid[x, y, z] = "#"
+    cube_grid = new_grid
+
+print(f"""AoC 2020 Day 17 Part 1 answer is: {np.count_nonzero(cube_grid == "#")}""")
+
+array_size = [cubes.shape[0] + padding * 2, cubes.shape[1] + padding * 2, padding * 2 + 1, padding * 2 + 1]
+cube_grid = np.full(array_size, '.', dtype=str)
+cube_grid[padding:array_size[0] - padding, padding:array_size[1] - padding, padding, padding] = cubes.copy()
+for a in range(6):
+    new_grid = np.full(array_size, '.', dtype=str)
+    for x in range(1, array_size[0] - 1):
+        for y in range(1, array_size[1] - 1):
+            for z in range(1, array_size[2] - 1):
+                for w in range(1, array_size[3] - 1):
+                    minicube = cube_grid[x - 1: x + 2, y - 1:y + 2, z - 1: z + 2, w - 1: w + 2]
+                    if (minicube[1, 1, 1, 1] == "#" and np.count_nonzero(minicube == "#") in [3, 4]) or (
+                            minicube[1, 1, 1, 1] == "." and np.count_nonzero(minicube == "#") == 3):
+                        new_grid[x, y, z, w] = "#"
+    cube_grid = new_grid
+
+print(f"""AoC 2020 Day 17 Part 2 answer is: {np.count_nonzero(cube_grid == "#")}""")

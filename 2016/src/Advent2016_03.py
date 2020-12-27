e89973625017774b4973da354294f13fbf1e49ec
david_buck <david.buck@ortex.com>
2020-12-27 08:52:01 +0000

    2016 Day 3 Complete

--- 2016/src/Advent2016_03.py
+++ 2016/src/Advent2016_03.py
@@ -3,9 +3,9 @@
 # From https://adventofcode.com/2016/day/3
 import numpy as np
 
-entries = [list(map(int, row.strip().split())) for row in open('../inputs/Advent2016_03.txt', 'r')]
-print(sum(x[0] < x[1] + x[2] and x[1] < x[0] + x[2] and x[2] < x[0] + x[1] for x in entries))
+triangles = np.array([list(map(int, row.strip().split())) for row in open('../inputs/Advent2016_03.txt', 'r')])
+print('AoC 2016 Day 3, Part 1 answer is ', sum(x[0] < x[1] + x[2] and x[1] < x[0] + x[2] and x[2] < x[0] + x[1] for x in triangles))
 
-triangles = np.array(entries).transpose()
+triangles = triangles.transpose()
 triangles = triangles.reshape(triangles.size//3, 3)
-print(sum(x[0] < x[1] + x[2] and x[1] < x[0] + x[2] and x[2] < x[0] + x[1] for x in triangles))
+print('AoC 2016 Day 3, Part 2 answer is ', sum(x[0] < x[1] + x[2] and x[1] < x[0] + x[2] and x[2] < x[0] + x[1] for x in triangles))

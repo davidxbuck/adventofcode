c0f6a8bd1f8dc4e68e6baad7a09c6117e00b05d0
david_buck <david.buck@ortex.com>
2020-12-28 07:15:54 +0000

    2016 Day 7 complete

--- 2016/src/Advent2016_07.py
+++ 2016/src/Advent2016_07.py
@@ -6,40 +6,41 @@ import re
 addresses = [re.findall(r'\w+', row.strip()) for row in open('../inputs/Advent2016_07.txt', 'r')]
 
 
-def palindrome_check(x):
-    for y in range(0, len(x) - 3):
-        if x[y] == x[y + 3] and x[y + 1] == x[y + 2] and x[y] != x[y + 1]:
+def palindrome_check(substr):
+    for y in range(0, len(substr) - 3):
+        if substr[y] == substr[y + 3] and substr[y + 1] == substr[y + 2] and substr[y] != substr[y + 1]:
             return True
+    return False
 
 
-valid_count = 0
+tls_count = 0
 for address in addresses:
     valid = False
-    for x in address[::2]:
+    for substring in address[::2]:
         if not valid:
-            valid = palindrome_check(x)
+            valid = palindrome_check(substring)
     if valid:
-        for x in address[1::2]:
-            if palindrome_check(x):
+        for substring in address[1::2]:
+            if palindrome_check(substring):
                 valid = False
                 break
     if valid:
-        valid_count += 1
+        tls_count += 1
 
-print(f"AoC 2016 Day 7, Part 1 answer is {valid_count}")
+print(f"AoC 2016 Day 7, Part 1 answer is {tls_count}")
 
-valid_count = 0
+ssl_count = 0
 for address in addresses:
     valid = False
     maybes = []
-    for x in address[::2]:
-        for y in range(len(x) - 2):
-            if x[y] == x[y + 2] and x[y] != x[y + 1]:
-                maybes.append(x[y + 1] + x[y] + x[y + 1])
-    for y in address[1::2]:
-        if any(z in y for z in maybes):
+    for substring in address[::2]:
+        for x in range(len(substring) - 2):
+            if substring[x] == substring[x + 2] and substring[x] != substring[x + 1]:
+                maybes.append(substring[x + 1] + substring[x] + substring[x + 1])
+    for x in address[1::2]:
+        if any(maybe in x for maybe in maybes):
             valid = True
             break
     if valid:
-        valid_count += 1
-print(f"AoC 2016 Day 7, Part 2 answer is {valid_count}")
+        ssl_count += 1
+print(f"AoC 2016 Day 7, Part 2 answer is {ssl_count}")

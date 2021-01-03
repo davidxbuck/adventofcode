4a6f44acf6eddfbbfca4a99ab6cff658a19cc9b9
David Buck <david.buck@datascience.fun>
2021-01-03 22:05:47 +0000

    2016 Day 17 complete

--- 2016/src/Advent2016_17.py
+++ 2016/src/Advent2016_17.py
@@ -4,17 +4,14 @@
 #
 import hashlib
 from queue import Queue
-from collections import defaultdict
 
 
 class Search:
-    def __init__(self):
+    def __init__(self, inp, part=1):
+        self.part = part
         self.searched = set()
-        self.prev = defaultdict(dict)
-        self.inputs = 'qtetzkpl'
-        # self.inputs = 'ihgpwlah'
-        # self.inputs = 'kglvqrro'
-        # self.inputs = 'ulqzkmiv'
+        self.longest = 0
+        self.inputs = inp
         self.q = Queue()
         self.q.put([1 + 1j, ""])
         self.pos = None
@@ -25,34 +22,44 @@ class Search:
                           "D": 0 + 1j,
                           "L": -1
                           }
+        self.so_far = None
         self.part1 = None
-        self.sofar = None
 
     def next_moves(self):
-        self.pos, self.sofar = self.q.get()
-        last = 'S'
-        if len(self.sofar) > 0:
-            last = self.sofar[-1]
+        self.pos, self.so_far = self.q.get()
         if self.pos == 4 + 4j:
-            self.found = True
-            return self.sofar
-        if self.pos in self.prev and last in self.prev[self.pos] and len(self.prev[self.pos][last]) < len(self.sofar):
-            return
-        self.prev[self.pos][last] = self.prev
-        self.key = hashlib.md5(bytes(self.inputs + self.sofar, encoding='utf8')).hexdigest()
-        for ix, dir in enumerate("UDLR"):
+            if self.part == 1:
+                self.found = True
+                return self.so_far
+            else:
+                self.longest = max([len(self.so_far), self.longest])
+                return
+        self.key = hashlib.md5(bytes(self.inputs + self.so_far, encoding='utf8')).hexdigest()
+        for ix, direction in enumerate("UDLR"):
             if self.key[ix] in "bcdef":
-                next_pos = self.pos + self.direction[dir]
+                next_pos = self.pos + self.direction[direction]
                 if 1 <= next_pos.real <= 4 and 1 <= next_pos.imag <= 4:
-                    if self.sofar + dir not in self.searched:
-                        self.searched.add(self.sofar + dir)
-                        self.q.put((next_pos, self.sofar + dir))
+                    if self.so_far + direction not in self.searched:
+                        self.searched.add(self.so_far + direction)
+                    self.q.put((next_pos, self.so_far + direction))
         return
 
     def search(self):
         while not self.found and not self.q.empty():
             self.part1 = self.next_moves()
-        return self.part1
+        if self.part == 1:
+            return self.part1
+        else:
+            return self.longest
 
-search = Search()
-print(search.search())
+
+inputs = 'qtetzkpl'
+# inputs = 'ihgpwlah'
+# inputs = 'kglvqrro'
+# inputs = 'ulqzkmiv'
+
+search = Search(inputs, 1)
+print(f"AoC 2016 Day 17 Part 1 answer is: {search.search()}")
+
+search = Search(inputs, 2)
+print(f"AoC 2016 Day 17 Part 2 answer is: {search.search()}")

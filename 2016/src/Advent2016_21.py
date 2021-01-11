0ea497fcf7ee47f80dad2c0540c77f2807586844
David Buck <david.buck@datascience.fun>
2021-01-11 09:20:52 +0000

    2016 Day 21 part 1 complete

---
+++ 2016/src/Advent2016_21.py
@@ -0,0 +1,45 @@
+# Advent of Code 2016
+#
+# From https://adventofcode.com/2016/day/21
+import re
+
+reg = r"""^(?:(swap position) (\d+) with position (\d+)|(swap letter) (\w) with letter (\w)|(rotate (?:right|left)) (\d+) steps?|(reverse) positions (\d+) through (\d)|(rotate based) on position of letter (\w)|(move) position (\d+) to position (\d+))$"""
+cmds = [[x for x in re.findall(reg, row.strip())[0] if x] for row in open('../inputs/Advent2016_21.txt', 'r')]
+
+inp = list('abcdefgh')
+
+for command in cmds:
+    cmd = command[0]
+
+    if cmd == "swap position":
+        x, y = map(int, command[1:])
+        inp[x], inp[y] = inp[y], inp[x]
+    elif cmd == "swap letter":
+        x, y = inp.index(command[1]), inp.index(command[2])
+        inp[x], inp[y] = inp[y], inp[x]
+    elif cmd == "reverse":
+        x, y = sorted(map(int, command[1:]))
+        if x == 0:
+            inp = inp[:x] + inp[y::-1] + inp[y + 1:]
+        else:
+            inp = inp[:x] + inp[y:x - 1:-1] + inp[y + 1:]
+    elif cmd == "move":
+        x, y = map(int, command[1:])
+        inp[y:y] = inp.pop(x)
+    elif cmd == "rotate right":
+        x = int(command[1])
+        inp = inp[len(inp) - x:] + inp[:len(inp) - x]
+    elif cmd == "rotate left":
+        x = int(command[1])
+        inp = inp[x:] + inp[:x]
+    elif cmd == "rotate based":
+        x = 1 + inp.index(command[1])
+        if x >= 5:
+            x += 1
+        inp = inp[len(inp) - x:] + inp[:len(inp) - x]
+    else:
+        print("Error", cmd)
+    print("".join(inp), command)
+print("".join(inp))
+
+inp2 = 'fbgdceah'

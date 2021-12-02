7e486208f456b8fdb363ef27a535b1ad6c2e1a00
David Buck <david.buck@datascience.fun>
2021-12-02 06:03:00 +0000

    2021 Day 2 complete

---
+++ 2021/src/Advent2021_02.py
@@ -0,0 +1,17 @@
+# Advent of Code 2021
+#
+# From https://adventofcode.com/2021/day/1
+#
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('../inputs/Advent2021_02.txt', names=['direction', 'distance'], sep=' ')
+df['part1_result'] = df['direction'].map({'forward': 1, 'down': 1j, 'up': -1j}) * df['distance']
+final_position = df['part1_result'].sum()
+
+print(f'Day 2, Part 1 {int(final_position.real * final_position.imag)}')
+
+df['aim'] = (df['direction'].map({'forward': 0, 'down': 1, 'up': -1}) * df['distance']).cumsum()
+df['part2_result'] = (real_result := np.real(df['part1_result'])) + real_result * df['aim'] * 1j
+final_position2 = df['part2_result'].sum()
+print(f'Day 2, Part 2 {int(final_position2.real * final_position2.imag)}')

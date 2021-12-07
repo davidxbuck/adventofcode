1655e8793448fb9e463332d29e71bbb4482dc616
David Buck <david.buck@datascience.fun>
2021-12-07 17:18:28 +0000

    2021 Day 7 Complete as single liners

--- 2021/src/Advent2021_07.py
+++ 2021/src/Advent2021_07.py
@@ -4,9 +4,5 @@
 #
 import numpy as np
 
-positions = np.array(list(map(int, [row.strip().split(',') for row in open('../inputs/Advent2021_07.txt', 'r')][0])))
-
-print(f'Day 7, Part 1 {int(np.sum(np.abs(positions - np.median(positions))))}')
-
-fuel = lambda x: x and x + fuel(x - 1) or 0
-print(f'Day 7, Part 2 {sum(fuel(abs(int(np.mean(positions)) - pos)) for pos in positions)}')
+print(f'Day 7, Part 1 {int(np.sum(np.abs((positions := np.array(list(map(int, [row.strip().split(",") for row in open("../inputs/Advent2021_07.txt", "r")][0])))) - np.median(positions))))}')
+print(f'Day 7, Part 2 {sum((fuel := lambda x: x and x + fuel(x - 1) or 0)(abs(int(np.mean(positions)) - pos)) for pos in positions)}')

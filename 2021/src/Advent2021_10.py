473ea0285771a230b32cbea085e22c5f8cfb77fd
David Buck <david.buck@datascience.fun>
2021-12-10 06:32:38 +0000

    Removed unused library

--- 2021/src/Advent2021_10.py
+++ 2021/src/Advent2021_10.py
@@ -2,7 +2,6 @@
 #
 # From https://adventofcode.com/2021/day/10
 #
-import math
 
 SCORES = {')': 3, ']': 57, '}': 1197, '>': 25137}
 AC_SCORES = {'(': 1, '[': 2, '{': 3, '<': 4}
@@ -14,6 +13,7 @@ errors = []
 auto_complete = []
 
 for ix, line in enumerate(lines):
+    stack = []
     incorrect = False
     for char in line:
         if char in '([{<':
@@ -31,4 +31,4 @@ for ix, line in enumerate(lines):
         auto_complete.append(total)
 
 print(f'Day 10, Part 1 {sum(SCORES[error] for error in errors)}')
-print(f'Day 10, Part 2 {sorted(auto_complete)[(len(auto_complete) - 1)//2]}')
+print(f'Day 10, Part 2 {sorted(auto_complete)[(len(auto_complete) - 1) // 2]}')

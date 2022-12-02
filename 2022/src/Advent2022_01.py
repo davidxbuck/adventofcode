a03dc8f76fc78f6cb238591bc581b2e563575262
David Buck <david.buck@datascience.fun>
2022-12-02 22:44:48 +0000

    2022 Day 2

--- 2022/src/Advent2022_01.py
+++ 2022/src/Advent2022_01.py
@@ -2,4 +2,7 @@
 #
 # From https://adventofcode.com/2022/day/1
 #
-print("Day 1, Part 1", max(data := [sum(map(int, x.split('\n'))) for x in open(f'../inputs/day1.txt', 'r').read().split('\n\n')]), '\n', "Day 1, Part 2", sum(sorted(data)[-3:]))
+data = [sum(map(int, x.split('\n'))) for x in open(f'../inputs/day1.txt', 'r').read().split('\n\n')]
+
+print(f'Day 1, Part 1 {max(data)}')
+print(f'Day 1, Part 2 {sum(sorted(data)[-3:])}')
--- 2022/src/Advent2022_02.py
+++ 2022/src/Advent2022_02.py
@@ -5,28 +5,10 @@
 data = [row.strip().split(' ') for row in open('../inputs/day2.txt', 'r')]
 
 choice = {'X': 1, 'Y': 2, 'Z': 3}
-game = {('A', 'X'): 3,
-        ('A', 'Y'): 6,
-        ('A', 'Z'): 0,
-        ('B', 'X'): 0,
-        ('B', 'Y'): 3,
-        ('B', 'Z'): 6,
-        ('C', 'X'): 6,
-        ('C', 'Y'): 0,
-        ('C', 'Z'): 3,
-        }
+game = {('A', 'X'): 3, ('A', 'Y'): 6, ('A', 'Z'): 0, ('B', 'X'): 0, ('B', 'Y'): 3, ('B', 'Z'): 6, ('C', 'X'): 6, ('C', 'Y'): 0, ('C', 'Z'): 3,}
 
+print(f'Day 2, Part 1 {sum(game[(x, y)] + choice[y] for x, y in data)}')
 choice2 = {'X': 0, 'Y': 3, 'Z': 6}
-game2 = {('A', 'X'): 3,
-         ('A', 'Y'): 1,
-         ('A', 'Z'): 2,
-         ('B', 'X'): 1,
-         ('B', 'Y'): 2,
-         ('B', 'Z'): 3,
-         ('C', 'X'): 2,
-         ('C', 'Y'): 3,
-         ('C', 'Z'): 1,
-         }
+game2 = {('A', 'X'): 3, ('A', 'Y'): 1, ('A', 'Z'): 2, ('B', 'X'): 1, ('B', 'Y'): 2, ('B', 'Z'): 3, ('C', 'X'): 2, ('C', 'Y'): 3, ('C', 'Z'): 1, }
 
-print(sum([game[(x, y)] + choice[y] for x, y in data]))
-print(sum([game2[(x, y)] + choice2[y] for x, y in data]))
+print(f'Day 2, Part 2 {sum(game2[(x, y)] + choice2[y] for x, y in data)}')
